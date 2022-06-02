f9b637c6abdcec622c31d1dd363ee1cd84c86734
Tan Yen Hui <yebhuitan@gmail.com>
2022-06-02 14:30:18 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppContext from './context/Context';
+import Navigator from './navigations/Navigator'
+import 'react-native-gesture-handler'
+
+export default function App() {
+  return (
+    <AppContext>
+      <Navigator/>
+    </AppContext>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
